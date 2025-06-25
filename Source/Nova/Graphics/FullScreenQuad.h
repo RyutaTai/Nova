44e96cd11c16cb2036560be8fc0e15d6e385272f
MSI\2220412 <z.ryu.d.0822@gmail.com>
2025-06-26 03:04:17 +0900

    引数の変数にアンダーバーついてたから消した。

--- Source/Nova/Graphics/FullScreenQuad.h
+++ Source/Nova/Graphics/FullScreenQuad.h
@@ -11,7 +11,7 @@ public:
 	FullScreenQuad(ID3D11Device* device);
 	virtual ~FullScreenQuad() = default;
 
-	void Blit(ID3D11DeviceContext* deviceContext, ID3D11ShaderResourceView** shaderResourceView_,
+	void Blit(ID3D11DeviceContext* deviceContext, ID3D11ShaderResourceView** shaderResourceView,
 		uint32_t startSlot, uint32_t numViews, ID3D11PixelShader* replacedPixelShader = nullptr);
 
 private:
